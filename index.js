cb03d2343e797ab29b11a3cb67e2ecc07dbc12fd
helloitsjoe <jboyle617@gmail.com>
2022-07-06 15:42:22 -0400

    working on floor

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+// TODO: Modules
 // import {Colors} from './utils.js'
 
 const canvas = document.getElementById('canvas');
-
-console.log('canvas', canvas);
 
 canvas.style.backgroundColor = 'black';
 
@@ -13,28 +12,35 @@ const Colors = {
   GREEN: 'lime',
   ORANGE: 'orange',
   YELLOW: 'goldenrod',
+  BROWN: 'brown',
 }
 
-const WIDTH = 400;
-const HEIGHT = 600;
+const WIDTH = 20;
+const HEIGHT = 30;
 const PIXEL = 20;
 
+// TODO: Increase as you level up
 let movesPerSecond = 1;
 
-// loop
-// draw
-// update
-
 const ctx = canvas.getContext('2d');
-console.log('ctx', ctx);
 
 let piece = createPiece(ctx);
-let floor = [];
+let floor = createFloor();
+console.log(floor);
+
+function createFloor() {
+  return Array(HEIGHT).fill([])
+    .map((_, row) => Array(WIDTH).fill(null)
+      .map((_, col) => {
+        const color = row === HEIGHT - 1 ? Colors.BROWN : null;
+        return createTile(ctx, color, row, col)
+      }))
+}
+
 const bg = {
   render: () => {
-    console.log('rendering');
     ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, WIDTH, HEIGHT)
+    ctx.fillRect(0, 0, WIDTH * PIXEL, HEIGHT * PIXEL)
   }
 }
 
@@ -44,10 +50,10 @@ const startLoop = () => {
   function loop() {
     id = requestAnimationFrame((elapsed) => {
       if (elapsed - prev > 1000) {
-        update(piece, floor)
-        draw(piece, floor, bg)
+        updateWorld(piece, floor)
         prev = elapsed
       }
+      drawWorld(piece, floor, bg)
       loop()
     });
   }
@@ -57,9 +63,9 @@ const startLoop = () => {
 }
 
 
-function update(piece, floor) {
+function updateWorld(piece, floor) {
   // Update falling piece
-  piece.update()
+  piece.update(floor)
   // If piece touches floor:
   //   add it to floor 
   //   get next shape from box
@@ -68,23 +74,25 @@ function update(piece, floor) {
   // End game if piece touches the top of the screen
 }
 
-function draw(piece, floor, bg) {
+function drawWorld(piece, floor, bg) {
   bg.render();
   piece.render();
-  floor.forEach((tile) => {
-    tile.render();
+  floor.forEach((row) => {
+    row.forEach((tile) => {
+      tile.render();
+    })
   });
 }
 
 function getShape() {
-  // TODO: create shapes
   const shapes = {
-    L: '' , 
-    T: '' , 
-    S: '' , 
-    Z: '' , 
-    I: '' , 
-    O: [['X', 'X'],['X', 'X']] , 
+    L: [[1, 0], [1, 0], [1, 1]], 
+    J: [[0, 1], [0, 1], [1, 1]], 
+    T: [[1, 0], [1, 1], [1, 0]], 
+    S: [[1, 0], [1, 1], [0, 1]], 
+    Z: [[0, 1], [1, 1], [1, 0]], 
+    I: [[1], [1], [1], [1]], 
+    O: [[1, 1],[1, 1]],
   }
 
   return shapes.O
@@ -116,26 +124,61 @@ function registerKeydown(fns) {
   return () => document.removeEventListener('keydown', cb);
 }
 
+function createTile(ctx, color, row, col) {
+  const isEmpty = !color;
+  const fill = color || 'rgba(255, 255, 255, 0)';
+
+  const render = () => {
+    ctx.fillStyle = fill;
+    ctx.fillRect(col * PIXEL, row * PIXEL, PIXEL, PIXEL)
+  }
+
+  return { render, isEmpty } 
+}
+
 function createPiece(ctx) {
   let pos = { x: WIDTH / 2, y: 0 }
+
   // TODO: Randomize
   const shape = getShape();
   const color = Colors.BLUE;
 
-  const moveRight = () => console.log('right!')
-  const moveLeft = () => console.log('left!')
-  const drop = () => console.log('drop!')
+  const moveRight = () => {
+    pos.x += 1;
+  }
+
+  const moveLeft = () => {
+    pos.x -= 1;
+  }
+
+  const drop = () => {
+    if (pos.y + 5 < HEIGHT) {
+      pos.y += 5;
+    }
+  }
 
   const cleanup = registerKeydown({moveRight, moveLeft, drop});
 
   const render = () => {
+    // console.log('pos', pos);
     ctx.fillStyle = color;
-    ctx.fillRect(pos.x, pos.y, PIXEL, PIXEL)
+    ctx.fillRect(pos.x * PIXEL, pos.y * PIXEL, PIXEL, PIXEL)
+    // shape.forEach((box) => {
+    //   if (!!box) {
+    //     ctx.fillRect(pos.x, pos.y, PIXEL, PIXEL)
+    //   }
+    // });
   }
 
-  const update = () => {
-    pos.y += (1 * PIXEL);
-    console.log('pos.y', pos.y);
+  const update = (floor) => {
+    // If !!floor[row][col] for any of the piece's boxes, stop the piece and move it to the floor array
+    // TODO: Will have to block horizontal movement
+    if (!floor[pos.y + 1]?.[pos.x].isEmpty) {
+      floor[pos.y][pos.x] = createTile(ctx, color, pos.y, pos.x)
+      reset()
+    } else {
+      pos.y += 1;
+    }
   }
 
   const reset = () => {
